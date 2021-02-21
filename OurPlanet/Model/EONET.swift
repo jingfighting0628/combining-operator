6d93d1ad9d4a160987e32e314b440d8cd84eda1c
liuningbo <liuningbo5166@163.com>
2021-02-21 09:00:23 +0800

    fix:要按category下载event，更新私有事件(forLast:closed:)方法,添加endpoint作为参数

--- OurPlanet/Model/EONET.swift
+++ OurPlanet/Model/EONET.swift
@@ -121,7 +121,7 @@ class EONET {
     
   }()
   
-  private static func events(forLast days :Int ,closed:Bool) -> Observable<[EOEvent]>{
+  private static func events(forLast days :Int ,closed:Bool ,endpoint :String) -> Observable<[EOEvent]>{
     
     let query :[String :Any] = [
       "days":days,
@@ -129,7 +129,7 @@ class EONET {
     
     ]
     
-    let request : Observable<[EOEvent]> = EONET.request(endpoint: eventsEndpoint, query: query,contentIdentifier: "events")
+    let request : Observable<[EOEvent]> = EONET.request(endpoint: endpoint  , query: query,contentIdentifier: "events")
     
     return request.catchErrorJustReturn([])
 
